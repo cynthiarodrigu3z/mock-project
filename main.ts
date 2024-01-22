29e829bd916c9ba5ff52c7409a23246cce362969
spoilmilked <156138163+spoilmilked@users.noreply.github.com>
2024-01-22 15:24:41 -0600

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,19 @@
 namespace SpriteKind {
     export const Item = SpriteKind.create()
 }
+scene.onOverlapTile(SpriteKind.Player, assets.tile`Tile0`, function (sprite, location) {
+    if (inventory.length > 0) {
+        game.showLongText("You picked up an item!", DialogLayout.Bottom)
+        // Perform any additional actions when the player overlaps with the tile
+        // For example, you might want to add the item to the inventory
+        addToInventory(inventory[0])
+    }
+})
 function addToInventory (item: Sprite) {
     inventory.push(item)
-}
-function displayInventory () {
-    for (let i of inventory) {
-        i.setPosition(x, 180)
-        x += 16
-    }
-}
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Item, function (sprite, item) {
-    addToInventory(item)
+    // Assuming you want to remove the item from the scene when added to the inventory
     item.destroy()
-    displayInventory()
-})
-let x = 0
+}
 let inventory: Sprite[] = []
 tiles.setCurrentTilemap(tilemap`lemap`)
 let mySprite = sprites.create(assets.image`duck`, SpriteKind.Player)
@@ -24,6 +22,6 @@ scene.cameraFollowSprite(mySprite)
 multilights.addLightSource(mySprite)
 multilights.toggleLighting(true)
 multilights.bandWidthOf(mySprite, 14)
-displayInventory()
-let key = sprites.create(assets.image`key`, SpriteKind.Item)
-tiles.placeOnRandomTile(key, assets.tile`Tile3`)
+// Example: Creating a key item
+let key = sprites.create(assets.image`myImage`, SpriteKind.Item)
+tiles.placeOnRandomTile(key, assets.tile`Tile0`)
