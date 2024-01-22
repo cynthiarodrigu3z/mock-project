0898d58135d1805edc93350272e0463136b31f84
spoilmilked <156138163+spoilmilked@users.noreply.github.com>
2024-01-22 16:27:18 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 namespace SpriteKind {
     export const Item = SpriteKind.create()
+    export const object = SpriteKind.create()
 }
 function addToInventory (item: Sprite) {
     let inventory: Sprite[] = []
@@ -11,23 +12,27 @@ let mySprite = sprites.create(assets.image`duck`, SpriteKind.Player)
 controller.moveSprite(mySprite)
 scene.cameraFollowSprite(mySprite)
 let key = sprites.create(assets.image`myImage`, SpriteKind.Item)
-let nmbpad = sprites.create(assets.image`numberpad`, SpriteKind.Item)
 tiles.placeOnRandomTile(key, assets.tile`Tile0`)
-tiles.placeOnRandomTile(nmbpad, assets.tile`Tile2`)
 multilights.addLightSource(mySprite)
 multilights.toggleLighting(true)
 multilights.bandWidthOf(mySprite, 14)
-forever(function () {
-    if (key.overlapsWith(mySprite)) {
-        story.spriteSayText(key, "1151")
-    }
-    if (nmbpad.overlapsWith(mySprite)) {
+game.onUpdateInterval(1000, function () {
+    if (mySprite.tileKindAt(TileDirection.Right, assets.tile`Tile2`)) {
         if (game.askForNumber("", 4) == 1151) {
             game.splash("correct!")
             scene.cameraShake(4, 500)
-            tiles.setCurrentTilemap(tilemap`map in doors`)
+            tiles.setCurrentTilemap(tilemap`lemap`)
         } else {
             game.splash("incorrect")
         }
+    }
+    if (mySprite.tileKindAt(TileDirection.Right, assets.tile`myTile0`)) {
+        scene.cameraShake(6, 500)
+        multilights.toggleLighting(false)
+    }
+})
+forever(function () {
+    if (key.overlapsWith(mySprite)) {
+        story.spriteSayText(key, "1151")
     }
 })
