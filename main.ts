ecb005d6fc3dfe1bcf5af630bcc4baac0ca17e14
spoilmilked <156138163+spoilmilked@users.noreply.github.com>
2024-01-22 15:59:55 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,35 @@
 namespace SpriteKind {
     export const Item = SpriteKind.create()
 }
-scene.onOverlapTile(SpriteKind.Player, assets.tile`Tile0`, function (sprite, location) {
-    if (inventory.length > 0) {
-        game.showLongText("You picked up an item!", DialogLayout.Bottom)
-        // Perform any additional actions when the player overlaps with the tile
-        // For example, you might want to add the item to the inventory
-        addToInventory(inventory[0])
-    }
-})
 function addToInventory (item: Sprite) {
+    let inventory: Sprite[] = []
     inventory.push(item)
-    // Assuming you want to remove the item from the scene when added to the inventory
     item.destroy()
 }
-let inventory: Sprite[] = []
-tiles.setCurrentTilemap(tilemap`lemap`)
+tiles.setCurrentTilemap(tilemap`map in doors`)
 let mySprite = sprites.create(assets.image`duck`, SpriteKind.Player)
 controller.moveSprite(mySprite)
 scene.cameraFollowSprite(mySprite)
+let key = sprites.create(assets.image`myImage`, SpriteKind.Item)
+let nmbpad = sprites.create(assets.image`numberpad`, SpriteKind.Item)
+tiles.placeOnRandomTile(key, assets.tile`Tile0`)
+tiles.placeOnRandomTile(nmbpad, assets.tile`Tile2`)
 multilights.addLightSource(mySprite)
 multilights.toggleLighting(true)
 multilights.bandWidthOf(mySprite, 14)
-// Example: Creating a key item
-let key = sprites.create(assets.image`myImage`, SpriteKind.Item)
-tiles.placeOnRandomTile(key, assets.tile`Tile0`)
+forever(function () {
+    if (key.overlapsWith(mySprite)) {
+        story.spriteSayText(key, "1151")
+    }
+    if (nmbpad.overlapsWith(mySprite)) {
+        if (game.askForNumber("", 4) == 1151) {
+            game.splash("correct!")
+            light.clear()
+            scene.cameraShake(4, 500)
+            tiles.setCurrentTilemap(tilemap`map in doors`)
+        } else {
+            game.splash("incorrect")
+            light.clear()
+        }
+    }
+})
