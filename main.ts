2bd13d861097e79997742f2e02a79d6574da564e
spoilmilked <156138163+spoilmilked@users.noreply.github.com>
2024-01-22 14:45:46 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,22 @@
-color.setPalette(
-color.GrayScale
-)
+namespace SpriteKind {
+    export const Item = SpriteKind.create()
+}
+function addToInventory (item: Sprite) {
+    inventory.push(item)
+}
+function displayInventory () {
+    for (let i of inventory) {
+        i.setPosition(x, 180)
+        x += 16
+    }
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Item, function (sprite, item) {
+    addToInventory(item)
+    item.destroy()
+    displayInventory()
+})
+let x = 0
+let inventory: Sprite[] = []
 tiles.setCurrentTilemap(tilemap`lemap`)
 let mySprite = sprites.create(assets.image`duck`, SpriteKind.Player)
 controller.moveSprite(mySprite)
@@ -8,3 +24,6 @@ scene.cameraFollowSprite(mySprite)
 multilights.addLightSource(mySprite)
 multilights.toggleLighting(true)
 multilights.bandWidthOf(mySprite, 14)
+displayInventory()
+let key = sprites.create(assets.image`key`, SpriteKind.Item)
+tiles.placeOnRandomTile(key, assets.tile`Tile3`)
