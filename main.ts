336e13914ffa574da5634d743309c4014bd8687d
spoilmilked <156138163+spoilmilked@users.noreply.github.com>
2024-01-24 16:10:11 -0600

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -6,6 +6,9 @@ namespace SpriteKind {
 namespace StatusBarKind {
     export const statusbar = StatusBarKind.create()
 }
+function endscene () {
+    scene.setBackgroundImage(assets.image`space`)
+}
 function statusBar () {
     statusbar = statusbars.create(30, 5, StatusBarKind.statusbar)
     statusbar.value = 0
@@ -22,10 +25,9 @@ function keyplacer () {
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.scrape, function (sprite, otherSprite) {
     if (true) {
-        sprites.destroy(scrap)
+        sprites.destroy(otherSprite, effects.spray, 500)
         mySprite.sayText("collected", 200, false)
-        tiles.placeOnRandomTile(scrap, assets.tile`Tile3`)
-        statusbar.value += 20
+        statusbar.value += 5
     }
 })
 function cutscene () {
@@ -45,13 +47,52 @@ function cutscene () {
     sprites.destroy(meteor)
     sprites.destroy(spaceship)
 }
-function placeScrap () {
-    scrap = sprites.create(assets.image`tattered metal sheet`, SpriteKind.scrape)
-    tiles.placeOnRandomTile(scrap, assets.tile`Tile3`)
+function placeScrap (num: number) {
+    for (let index = 0; index < num; index++) {
+        if (100 > statusbar.value) {
+            list = [sprites.create(assets.image`tattered metal sheet`, SpriteKind.scrape), sprites.create(img`
+                ..............bbbbbbb...........
+                ...........bb66663333baa........
+                .........bb3367776333663aa......
+                ........b33333888333389633aa....
+                .......b3333333333333389633aa...
+                ......b34443333333333338633bae..
+                .....b3455433333333334443333ae..
+                ....b33322333dddd3333455233daee.
+                ...b3d333333dd3bbbb33322333dabe.
+                ..b3d333333d3bb33bb33333333da4e.
+                ..bd33333333b33aab3333333223a4ee
+                .b3d3663333b33aab33366332442b4ee
+                .bd3b983333a3aa3333387633ee3b4ee
+                .bd6983333baaa333333387633bb4bee
+                b3d6833333bba333333333863ba44ebe
+                bdd3333333bb3333333333333a44bebe
+                add666633333322333366333ba44bbbe
+                ad67776333332442336983d3a444b4e.
+                add888b333333ee3369833d3a44b44e.
+                add333333333333336833d3a444b4e..
+                a3dd3333344433333dddd3a444b44e..
+                ab33ddd325543333dd33aa444b44e...
+                .eabb3dd32233333baaa4444b44e....
+                .ebabb3d333d33baa444443b44e.....
+                ..ebaab3ddd3aaa4444433b44e......
+                ..eebbaab33a44444333b444e.......
+                ...eeebbaab444b333b4444e........
+                ....ebeeebbbbbbbb4444ee.........
+                .....eebbbb44444444ee...........
+                .......eeebbb444eee.............
+                ..........eeeeee................
+                ................................
+                `, SpriteKind.scrape), sprites.create(assets.image`tattered metal sheet`, SpriteKind.scrape)]
+            tiles.placeOnRandomTile(list._pickRandom(), assets.tile`Tile3`)
+        } else {
+            endscene()
+        }
+    }
 }
+let list: Sprite[] = []
 let spaceship: Sprite = null
 let meteor: Sprite = null
-let scrap: Sprite = null
 let statusbar: StatusBarSprite = null
 let key: Sprite = null
 let mySprite: Sprite = null
@@ -74,7 +115,7 @@ game.onUpdateInterval(1000, function () {
             game.splash("access granted")
             scene.cameraShake(8, 500)
             tiles.setCurrentTilemap(tilemap`lemap`)
-            placeScrap()
+            placeScrap(1)
             statusBar()
         } else {
             game.splash("access denied")
