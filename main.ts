3763a133302d377abcc2cb08f06d4769e716d013
spoilmilked <156138163+spoilmilked@users.noreply.github.com>
2024-01-31 23:41:03 -0600

    Restore '1/31/2024, 11:04:35 PM Update main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -6,15 +6,6 @@ namespace SpriteKind {
 namespace StatusBarKind {
     export const statusbar = StatusBarKind.create()
 }
-sprites.onCreated(SpriteKind.Enemy, function (sprite) {
-    ENEMIE.follow(mySprite, 20)
-})
-function spawn_enemies (numEnemies: number) {
-    for (let index = 0; index < numEnemies; index++) {
-        ENEMIE = sprites.create(assets.image`alien`, SpriteKind.Enemy)
-        tiles.placeOnRandomTile(ENEMIE, assets.tile`Tile3`)
-    }
-}
 function openDoorWithPin (code: number) {
     if (code == 1151) {
         // Access granted
@@ -22,20 +13,6 @@ function openDoorWithPin (code: number) {
     } else {
         // Access denied
         return false
-    }
-}
-function place (num: number) {
-    totalTrash = Math.floor(100 / num) + 1
-    for (let index = 0; index < totalTrash; index++) {
-        let list: Sprite[] = []
-        scrap = sprites.create(assets.image`tattered metal sheet`, SpriteKind.scrape)
-        list.push(scrap)
-        tiles.placeOnRandomTile(scrap, assets.tile`Tile3`)
-    }
-    if (10 < num) {
-        spawn_enemies(num / 2)
-    } else {
-        spawn_enemies(num / 3)
     }
 }
 function keyplacer () {
@@ -68,22 +45,26 @@ function cutscene () {
     sprites.destroy(meteor)
     sprites.destroy(spaceship)
 }
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    info.changeLifeBy(-1)
-})
+function placeScrap (num: number) {
+    totalTrash = Math.floor(100 / num) + 1
+    for (let index = 0; index < totalTrash; index++) {
+        let list: Sprite[] = []
+        scrap = sprites.create(assets.image`tattered metal sheet`, SpriteKind.scrape)
+        list.push(scrap)
+        tiles.placeOnRandomTile(scrap, assets.tile`Tile3`)
+    }
+}
 let pin = 0
+let scrap: Sprite = null
 let spaceship: Sprite = null
 let meteor: Sprite = null
-let collectedTrash = 0
-let scrap: Sprite = null
 let totalTrash = 0
-let ENEMIE: Sprite = null
+let collectedTrash = 0
 let key: Sprite = null
-let mySprite: Sprite = null
 cutscene()
 keyplacer()
 tiles.setCurrentTilemap(tilemap`map in doors`)
-mySprite = sprites.create(assets.image`duck`, SpriteKind.Player)
+let mySprite = sprites.create(assets.image`duck`, SpriteKind.Player)
 scene.cameraFollowSprite(mySprite)
 controller.moveSprite(mySprite)
 let mySprite2 = sprites.create(assets.image`monkey`, SpriteKind.Player)
@@ -91,7 +72,6 @@ multilights.addLightSource(mySprite, 14)
 multilights.addLightSource(mySprite2, 14)
 multilights.toggleLighting(true)
 tiles.placeOnRandomTile(key, assets.tile`Tile0`)
-info.setLife(3)
 game.showLongText("The powers out....", DialogLayout.Bottom)
 game.showLongText("We need to go to the power generator and turn on the emergency power", DialogLayout.Bottom)
 game.showLongText("im sure i wrote the code for the door down somewhere", DialogLayout.Bottom)
@@ -103,7 +83,7 @@ game.onUpdateInterval(1000, function () {
             game.splash("access granted")
             scene.cameraShake(8, 500)
             tiles.setCurrentTilemap(tilemap`lemap`)
-            place(game.askForNumber("difficulty level 1-20", 2))
+            placeScrap(game.askForNumber("difficulty level 1-20", 2))
             game.splash("the door opened! lets go turn on the power")
         } else {
             game.splash("access denied")
